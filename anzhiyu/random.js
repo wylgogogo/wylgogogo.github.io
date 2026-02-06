53f4ff49fe88f708bd63bfabfbe9cbb6c5dba512
oYo <1404075791@qq.com>
2026-02-06 15:40:18 +0800

    Site updated: 2026-02-06 15:40:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/06/python协程记录/"];function toRandomPost(){
+var posts=["2026/02/06/VTTP-虚拟化拓扑平台/","2026/02/06/python协程记录/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
