cd74121f5463b715d58c39ce853c4c27d39a9641
oYo <1404075791@qq.com>
2026-02-06 10:55:20 +0800

    Site updated: 2026-02-06 10:55:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/05/hello-world/"];function toRandomPost(){
+var posts=["2026/02/05/hello-world/","2026/02/06/python协程记录/","2026/02/06/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
